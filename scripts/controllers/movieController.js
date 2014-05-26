791043bda6c905050f547015a5572909a083d35c
b.marinov <b.marinov@fio.de>
2014-05-26 10:48:31 +0300

    Some cleanup and added appropriately to VS project.

--- scripts/controllers/movieController.js
+++ scripts/controllers/movieController.js
@@ -14,6 +14,7 @@
             })
             .success(function (data, status, headers, config) {
                 $scope.movies = data.Search;
+                console.log(headers);
                 $scope.showMovieResults = true;
                 $scope.showErrorMessage = false;
             })
